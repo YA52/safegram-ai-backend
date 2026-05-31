995be35496d65506ea8e3c6b729c978f750a74fa
harshi <87275652+YA52@users.noreply.github.com>
2026-05-31 20:52:29 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/analyze", (req, res) => {
+
+    const comment = req.body.comment.toLowerCase();
+
+    const badWords = [
+        "fake",
+        "fraud",
+        "scam",
+        "idiot",
+        "stupid",
+        "hate",
+        "waste",
+        "vedhava",
+        "chetta"
+    ];
+
+    let score = 100;
+
+    badWords.forEach(word => {
+        if (comment.includes(word)) {
+            score -= 20;
+        }
+    });
+
+    if (score < 0) score = 0;
+
+    let status;
+
+    if (score >= 80) {
+        status = "SAFE";
+    } else if (score >= 50) {
+        status = "WARNING";
+    } else {
+        status = "DANGEROUS";
+    }
+
+    res.json({
+        score,
+        status
+    });
+
+});
+
+app.listen(5000, () => {
+    console.log("SafeGram AI Backend Running on Port 5000");
+});
